f65363ceca156286cfdf7c4c978026106d201095
Tyler <tgallup@berkeley.edu>
2023-10-19 17:06:01 -0700

    Pushing for github pages

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const port = 80;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/styles.css', (req, res) => {
-  res.sendFile(__dirname + '/styles.css');
+app.get("/styles.css", (req, res) => {
+  res.sendFile(__dirname + "/styles.css");
 });
 
-app.get('/styles.css', (req, res) => {
-  res.sendFile(__dirname + '/script.js');
+app.get("/styles.css", (req, res) => {
+  res.sendFile(__dirname + "/script.js");
 });
-
 
 app.listen(port, () => {
   console.log(`Tyler Gallup's Website listening at http://localhost:${port}`);
